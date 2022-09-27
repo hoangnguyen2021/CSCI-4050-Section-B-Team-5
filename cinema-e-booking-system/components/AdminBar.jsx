8593a7c769c68cd0bd541fe074865a2f2141aec4
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-09-27 18:28:43 -0400

    Admin navbar

---
+++ cinema-e-booking-system/components/AdminBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from 'next/link'
+export const Navbar = () => {
+  return (
+    <div className="navigation-tabs">
+      <li>
+      <h1><a href="/manageUsers">Users/Admins</a></h1>
+      </li>
+      <li>
+        <h1><a href="/manageMovies">Manage Movies</a></h1>
+      </li>
+      <li>
+      <h1><a href="/promotions">Promotions</a></h1>
+      </li>
+      <li>
+      <h1><a href="/">Home</a></h1>
+      </li>
+    </div>
+  );
+};
