19d1a21151f9b6b4820c92b838ba6b9a896bc239
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-09-27 22:18:42 -0400

    Imported new styles

--- cinema-e-booking-system/pages/_app.js
+++ cinema-e-booking-system/pages/_app.js
@@ -1,4 +1,7 @@
 import "../styles/globals.css";
+import "../styles/adminBar.css";
+import "../styles/editprofile.css";
+import "../styles/manageUsers.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
