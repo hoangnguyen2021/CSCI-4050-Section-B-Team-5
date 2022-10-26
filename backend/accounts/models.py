7bea4112d88609f880a05febfa0c71060200d843
yuvateja <yuvateja88@gmail.com>
2022-10-25 21:04:21 -0400

    working backend

---
+++ backend/accounts/models.py
@@ -0,0 +1,36 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self,email,name , password=None):
+        if not email:
+            raise ValueError('Users Must have an email')
+        email = self.normalize_email(email)
+        user = self.model(email=email , name=name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    # def create_super_user():
+
+
+class UserAccount(AbstractBaseUser , PermissionsMixin ):
+    email = models.EmailField(max_length = 225 , unique=True)
+    name = models.CharField(max_length = 225)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserAccountManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+   
+
+    def get_name(self):
+        return self.name
+    def __str__(self):
+        return self.email
---
+++ backend/accounts/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+User = get_user_model()
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'name','email', 'password')
