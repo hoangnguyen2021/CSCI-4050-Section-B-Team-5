b0137ea021ac04fca2cd2e70bc889da34fde0ed9
Hoang Nguyen <vuhoangnguyenga@gmail.com>
2022-10-26 14:11:48 -0400

    Fix change password

--- cinema-e-booking-system/components/TopNavigation.jsx
+++ cinema-e-booking-system/components/TopNavigation.jsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 import Link from "next/link";
 import Modal from "../components/Modal";
-import InputField from "../components/InputField";
-import PhoneNumberField from "./PhoneNumberField";
-import EmailField from "./EmailField";
-import PillButton from "./PillButton";
-import { EditSvg } from "./Svg";
+import EditProfileForm from "./EditProfileForm";
 
 const TopNavigation = () => {
   const [open, setOpen] = useState(false);
-  const [nameRO, setNameRO] = useState(true);
-  const [phoneNumberRO, setPhoneNumberRO] = useState(true);
-  const [emailRO, setEmailRO] = useState(true);
 
   return (
     <div className="bg-background">
@@ -26,36 +19,10 @@ const TopNavigation = () => {
             onClick={() => setOpen(true)}
             className="text-sm font-medium text-on-primary cursor-pointer hover:text-gray-100"
           >
-            Account
+            Profile
           </div>
           <Modal open={open} setOpen={setOpen} title="Edit Profile">
-            <div className="flex flex-col gap-y-5">
-              <div className="flex justify-between space-x-3 items-end">
-                <InputField label="Name" readOnly={nameRO} />
-                <button onClick={() => setNameRO(false)}>
-                  <EditSvg />
-                </button>
-              </div>
-              <div className="flex justify-between space-x-3 items-end">
-                <PhoneNumberField
-                  label="Phone Number"
-                  readOnly={phoneNumberRO}
-                />
-                <button onClick={() => setPhoneNumberRO(false)}>
-                  <EditSvg />
-                </button>
-              </div>
-              <div className="flex justify-between space-x-3 items-end">
-                <EmailField label="Email" readOnly={emailRO} />
-                <button onClick={() => setEmailRO(false)}>
-                  <EditSvg />
-                </button>
-              </div>
-
-              <div className="pt-10">
-                <PillButton text="Save" />
-              </div>
-            </div>
+            <EditProfileForm />
           </Modal>
         </div>
       </div>
