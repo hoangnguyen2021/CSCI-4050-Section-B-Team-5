7f00ecb3b1e90792948ff11d30dddd4143528e5b
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-10-26 23:14:34 -0400

    Add my login

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,55 +1,140 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import BackgroundOverlay from "../../components/BackgroundOverlay";
-import EmailField from "../../components/EmailField";
-import PasswordField from "../../components/PasswordField";
-import Button from "../../components/Button";
+import Link from 'next/link'
+import React,{useState} from 'react' 
+function Login() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [rememberMe, setRememberMe] = useState(0);
 
-const LoginPage = () => {
-  return (
-    <div className="bg-background">
-      <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <Link href="/">
-          <div className="text-sm font-medium text-on-primary cursor-pointer hover:text-gray-100">
-            Back to Home
-          </div>
-        </Link>
-      </div>
-      <div className="relative min-h-screen">
-        <BackgroundOverlay
-          src="https://wallpapercave.com/wp/wp6608538.jpg"
-          opacity={70}
-        />
-        <div className="relative flex justify-center items-center pt-20">
-          <div className="basis-1/4 flex flex-col space-y-3 bg-background-variant rounded-lg shadow-md px-8 py-10">
-            <h2 className="text-3xl font-semibold text-center pb-5">
-              <span className="text-primary">Bulldog</span>
-              <span> </span>
-              <span className="text-on-primary">Cinema</span>
-            </h2>
-            <h3 className="text-xl font-semibold text-center">Sign In</h3>
-            <EmailField placeholder="Email" />
-            <PasswordField placeholder="Password" />
-            <Link href="/admin-login">
-              <div className="text-base font-medium text-primary cursor-pointer hover:text-primary-variant">
-                Admin Portal
-              </div>
-            </Link>
+  // User Login info
+  const users = [
+    {
+      email: "user1",
+      password: "pass1"
+    },
+    {
+      email: "user2",
+      password: "pass2"
+    }
+  ];
 
-            <div className="pt-10 flex justify-center">
-              <Button text="Sign in" />
-            </div>
-            <div className="text-base font-medium text-center">
-              <span className="text-on-primary">Don't have an account? </span>
-              <Link href="/register">
-                <span className="text-primary cursor-pointer">Join</span>
-              </Link>
-            </div>
+  const errors = {
+    email: "invalid email",
+    password: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { email, password } = document.forms[0];
+
+    // Find user login info
+    const userData = users.find((user) => user.email === uname.password);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+        } else if (userData.email !== email.value){
+      // Username not found
+          setErrorMessages({ name: "email", message: errors.email });
+          } else {
+            setIsSubmitted(true);
+          } // else
+      } // if
+    }; // if
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+// Remember Me
+    let handleCheckbox = async (e) => {
+      if (rememberMe == 0) {
+        setRememberMe(1);
+      } else {
+        setRememberMe(0);
+      }
+    };
+
+  // Code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <div className="label text-center">
+          <label>Sign In</label>
           </div>
+          <input type="text" placeholder = "Email" name="email" required />
+          {renderErrorMessage("email")}
         </div>
-      </div>
+        <div className="input-container">
+          <input type="password" placeholder = "Password" name="password" required />
+          {renderErrorMessage("password")}
+        </div>
+        <div className="text-sm">
+          <Link href="/adminLogin">
+                <a className="font-medium text-red-600 hover:text-red-500">
+                  Admin Portal
+                </a>
+                </Link>
+              </div>
+              <div className="newAccount">
+                
+              </div>
+        <div className="button-container">
+          <input type="submit"/>
+        </div>
+        <div className="text-center">
+              <span>Remember Me</span>
+              <input type="checkbox" onChange={handleCheckbox}/>
+              </div>
+        <div className="createSpan text-center">
+          <span className="text-on-primary">Don't have an account? </span>
+          <Link href="/register">
+          <button className="text-primary"> Join </button>
+          </Link>
+        </div>
+      </form>
     </div>
   );
-};
 
-export default LoginPage;
+  return (
+<div className="bg-background">
+<div className="back-to-home">
+        <Link href="/">
+    <button className="home"> Back to Home</button>
+    </Link>
+</div>
+      <div className="relative">
+        {/* Decorative image and overlay */}
+        <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
+          <img
+            src="https://wallpapercave.com/wp/wp6608538.jpg"
+            alt=""
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 bg-background opacity-70"
+        />
+    <div className="login">
+      <div className="login-form">
+        <div className="title text-center">
+        <span className="text-primary">Bulldog</span>
+            <span> </span>
+            <span className="text-on-primary">Cinema</span>
+        </div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default Login;
