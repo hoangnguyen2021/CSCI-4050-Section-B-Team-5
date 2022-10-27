ef5bdf607f7d9892646bc30f9917bb4531e32145
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-10-27 14:15:36 -0400

    Added styles

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import "../styles/styles.css";
 import "../styles/manageUsers.css";
+import "../styles/editProfile.css";
+import "../styles/adminBar.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
