d777732db8edfebab17baad6921a26e932a731a4
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-10-26 23:17:53 -0400

    Updated editProfile

--- frontend/pages/editProfile.js
+++ frontend/pages/editProfile.js
@@ -1,14 +1,18 @@
 import Link from 'next/link'
 import React from 'react';
 
-
-
-
-
 export default function EditProfiles() {
-  const onlyNumbers = e => {
-    e.target.value = e.target.value.replace(/\D/g, '')
-  }
+
+  const [promotion, setPromotion] = useState(0);
+
+  let handleCheckbox = async (e) => {
+
+    if (promotion == 0) {
+      setPromotion(1);
+    } else {
+      setPromotion(0);
+    }
+  };
         return (
           <div className="edit-profiles">
                     <div className="back-to-home">
@@ -22,16 +26,28 @@ export default function EditProfiles() {
               <h1 className="text-center">Update your Information</h1>
               <h3>Profile Info</h3>
               <div>
-              <input className="fields" type="text" placeholder="New First Name"/>
+              <input className="fields" type="text" placeholder="New Name"/>
               </div>
               <div>
-              <input className="fields" type="text" placeholder="New Last Name" />
+              <input className="fields" type="text" placeholder="Billing Address" />
               </div>
               <div>
               <input className="fields" type="text" placeholder="Email" />
               </div>
               <div>
-              <input onInput={onlyNumbers} className="fields" type="text" placeholder="New Phone Number"/>
+              <input className="fields" type="text" placeholder="New Phone Number"/>
+              </div>
+              <div>
+              <input className="fields" type="text" placeholder="Card Number"/>
+              </div>
+              <div>
+              <input className="fields" type="text" placeholder="Expiration Date"/>
+              </div>
+              <div>
+              <input className="fields" type="text" placeholder="CVV"/>
+              </div>
+              <div>
+              <input className="fields" type="text" placeholder="Zipcode"/>
               </div>
               <div className="createSpan text-center">
           <Link href="/changePassword">
@@ -40,9 +56,8 @@ export default function EditProfiles() {
         </div>
               <div>
               <span>Subscribe to Promotions</span>
-              <input  type="checkbox"/>
+              <input  type="checkbox" onChange={handleCheckbox}/>
               </div>
-
               <h3 id="incorrect-credentials" style={{color: 'red'}}></h3>
               <button id="buttonstyle" type="submit">Update account information</button>
               </div>
