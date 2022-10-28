a4582ca214472c82e2ae43930e066a1d9e1a6dda
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-10-27 23:31:59 -0400

    added card information

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,28 +6,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self,email, password=None , **extra_fields):
+    def create_user(self,email,name , password=None):
         if not email:
             raise ValueError('Users Must have an email')
         email = self.normalize_email(email)
-        user = self.model(email=email ,**extra_fields )
+        user = self.model(email=email , name=name)
         user.set_password(password)
         user.save()
         return user
 
     # def create_super_user():
 
+    def edit_user(self, email, name, password):
+        
+        return user
 
 class UserAccount(AbstractBaseUser , PermissionsMixin ):
     email = models.EmailField(max_length = 225 , unique=True)
     name = models.CharField(max_length = 225)
-    phonenumber = models.CharField(max_length = 12)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default=False)
+    cardnum = models.CharField(max_length = 16)
+    cvv = models.CharField(max_length = 3)
+    expiration_year = models.CharField(max_length = 4)
+    zip_code = models.CharField()
     objects = UserAccountManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phonenumber']
+    REQUIRED_FIELDS = ['name']
 
    
 
