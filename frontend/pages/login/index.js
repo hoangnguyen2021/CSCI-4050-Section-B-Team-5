47c5399e2807fd395349f1457b67c148bfb35f4c
Hoang Vu Nguyen <hoangvu.nguyen@washpost.com>
2022-10-27 20:45:17 -0400

    Registration, login, reset password implemented

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,5 +1,7 @@
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
 import { useFetch } from "../../hooks/useFetch";
 import BackgroundOverlay from "../../components/BackgroundOverlay";
 import EmailField from "../../components/EmailField";
@@ -10,15 +12,63 @@ const LoginPage = () => {
   const { post } = useFetch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { uid, token } = router.query;
+
+  useEffect(() => {
+    if (uid && token) {
+      activateUser();
+    }
+  }, [uid, token]);
+
+  const activateUser = async () => {
+    try {
+      const response = await post("auth/users/activation/", {
+        uid: uid,
+        token: token,
+      });
+      toast.success("Your account is activated!");
+      const responseData = response.data;
+      console.log(responseData);
+    } catch (error) {
+      const responseData = error.response?.data;
+      if (responseData) {
+        if (responseData.detail) {
+          toast.error(responseData.detail);
+        }
+      } else {
+        toast.error("Cannot activate your account!");
+      }
+      //console.error(error);
+    }
+  };
 
   const loginUser = async (e) => {
     e.preventDefault();
-    const response = await post("auth/jwt/create/", {
-      email: email,
-      password: password,
-    });
-    console.log(response.data);
-    // TODO: redirect to homepage, store token in local storage
+    try {
+      const response = await post("auth/jwt/create/", {
+        email: email,
+        password: password,
+      });
+      toast.success("You are logged in!");
+      router.push('/');
+      const responseData = response.data;
+      if (responseData) {
+        localStorage.setItem("refresh", responseData.refresh);
+        localStorage.setItem("access", responseData.access);
+      }
+      console.log(responseData);
+    } catch (error) {
+      const responseData = error.response?.data;
+      if (responseData) {
+        if (responseData.detail) {
+          toast.error(responseData.detail);
+        }
+      } else {
+        toast.error("Login error!");
+      }
+      console.log(error);
+    }
   };
 
   return (
@@ -36,7 +86,7 @@ const LoginPage = () => {
           opacity={70}
         />
         <div className="relative flex justify-center items-center pt-20">
-          <div className="basis-1/4 flex flex-col space-y-3 bg-background-variant rounded-lg shadow-md px-8 py-10">
+          <div className="basis-1/4 flex flex-col items-center bg-background-variant rounded-lg shadow-md px-8 py-10">
             <h2 className="text-3xl font-semibold text-center pb-5">
               <span className="text-primary">Bulldog</span>
               <span> </span>
@@ -53,7 +103,7 @@ const LoginPage = () => {
                 </div>
               </Link>
 
-              <div className="pt-10 flex justify-center">
+              <div className="pt-10 pb-5 flex justify-center">
                 <SubmitButton text="Sign in" />
               </div>
             </form>
@@ -62,6 +112,13 @@ const LoginPage = () => {
               <span className="text-on-primary">Don't have an account? </span>
               <Link href="/register">
                 <span className="text-primary cursor-pointer">Join</span>
+              </Link>
+            </div>
+
+            <div className="text-base font-medium text-center">
+              <span className="text-on-primary">Forgot Password? </span>
+              <Link href="/reset-password">
+                <span className="text-primary cursor-pointer">Reset</span>
               </Link>
             </div>
           </div>
