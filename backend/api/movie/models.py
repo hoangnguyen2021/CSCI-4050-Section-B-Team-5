dbbb1d22c8ddba4f5792bf0ba2549d7c78f86ab2
Noyon-Dey <49205445+Noyon-Dey@users.noreply.github.com>
2022-11-06 16:17:53 -0500

    created models and serializers for movies and promotions

--- backend/api/movie/models.py
+++ backend/api/movie/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length = 100)
+    #movie_category = models.IntegerField() //it can be a string Horror, only a type for a movie
+    movie_category = models.CharField(max_length = 100)
+
+    movie_cast = models.CharField(max_length = 300)
+    director = models.CharField(max_length = 100)
+    producer = models.CharField(max_length = 300)
+    synopsis = models.CharField(max_length = 300)
+    reviews = models.CharField(max_length = 300)
+    trailer_pic = models.ImageField(upload_to = 'pics/')
+    trailer_video = models.FileField(upload_to='videos/', null=True, verbose_name="")
+    rating = models.FloatField()
+    show_date = models.DateField() 
+    #// showroom table, movie id will be a foreign key
+    show_time = models.TimeField() 
+    #// showroom table, movie id will be a foreign key
+
+    ticket_price = models.FloatField()
+    is_active = models.BooleanField()  
+    movie_duration = models.FloatField()
