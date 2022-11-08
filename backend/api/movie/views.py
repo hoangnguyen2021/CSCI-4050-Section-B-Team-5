91f5ba11587516283b870c7c39d4df13af090a5e
kaczynski-tech-tips <nameissubjecttochange@gmail.com>
2022-11-08 14:57:03 -0500

    various movie models and temp change promo model

--- backend/api/movie/views.py
+++ backend/api/movie/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Movie
+from .serializers import MovieSerializers 
 
 # Create your views here.
 
