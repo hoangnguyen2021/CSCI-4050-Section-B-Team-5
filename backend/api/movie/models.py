9d5295053453557c53b2491479ceb70761691f80
Guvvala-Prasanth-Reddy <qtpython12@gmail.com>
2022-11-09 18:54:14 -0500

    model for Movie class

--- backend/api/movie/models.py
+++ backend/api/movie/models.py
@@ -12,14 +12,8 @@ class Movie(models.Model):
     producer = models.CharField(max_length = 300)
     synopsis = models.CharField(max_length = 300)
     reviews = models.CharField(max_length = 300)
-    trailer_pic = models.ImageField(upload_to = 'pics/')#this has to be a hyperlink field (models.URLField)
-    trailer_video = models.FileField(upload_to='videos/', null=True, verbose_name="")  #same goes in here
-    rating = models.FloatField()
-    show_date = models.DateField() 
-    #// showroom table, movie id will be a foreign key
-    show_time = models.TimeField() 
-    #// showroom table, movie id will be a foreign key
-
-    ticket_price = models.FloatField()
+    trailer_pic = models.URLField()
+    trailer_video = models.URLField()
+    rating = models.IntegerField(max = 3)
     is_active = models.BooleanField()  
-    movie_duration = models.FloatField()
+    movie_duration = models.DurationField()
