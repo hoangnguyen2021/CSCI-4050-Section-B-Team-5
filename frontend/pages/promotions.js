4f7c2758c880ccee45f10985666410293d335b63
shaninanje <83554367+shaninanje@users.noreply.github.com>
2022-11-10 20:57:41 -0500

    Update promotions.js

--- frontend/pages/promotions.js
+++ frontend/pages/promotions.js
@@ -1,65 +1,65 @@
 import { Navbar } from "../components/AdminBar";
+import { useEffect, useState } from "react";
 import React from 'react';
+import Modal from "../components/Modal";
+import AddPromotionForm from "../components/AddPromotionForm";
 
 export default function Promotions() {
-
-          return (
+  const [open, setOpen] = useState(false);
+          
+  return (
             <div className="admin-promo">
         <Navbar />
 
 <div className="text-center">Promotions</div>
-
+<div
+            onClick={() => setOpen(true)}
+            className="text-sm text-center font-medium text-on-primary cursor-pointer hover:text-gray-100"
+          >
+            Add Promo
+          </div>
+          <Modal open={open} setOpen={setOpen} title="Add Promo">
+            <AddPromotionForm />
+          </Modal>
 <table>
   <tbody>
   <tr>
     <th>Promotion</th>
     <th>Code</th>
     <th>Expiration Date</th>
+    <th>Percentage</th>
     <th></th>
   </tr>
   <tr>
     <td>BOGO 10%</td>
     <td>EXG62FG</td>
     <td>10/20/2022</td>
+    <th>10%</th>
     <td><button id="button" type="submit">Delete</button></td>
   </tr>
   <tr>
     <td>20% off children's ticket</td>
     <td>20KIDS</td>
     <td>10/20/2022</td>
+    <th>20%</th>
     <td><button id="button" type="submit">Delete</button></td>
   </tr>
   <tr>
     <td>15% off Military Discount</td>
     <td>15MILITARY</td>
     <td>10/20/2022</td>
+    <th>15%</th>
     <td><button id="button" type="submit">Delete</button></td>
   </tr>
   <tr>
     <td>25% Valentines Day</td>
     <td>BEMINEFOR25</td>
     <td>02/15/2023</td>
+    <th>25%</th>
     <td><button id="button" type="submit">Delete</button></td>
   </tr>
   </tbody>
 </table>
-<form id="addPromo">
-        <div className="promo-container">
-          <div className="newPromoLabel">
-          <label>Add new promotion?</label>
-          </div>
-          <input type="text" placeholder = "New Promotion" name="promo" required />
-        </div>
-        <div className="promo-container">
-          <input type="text" placeholder = "Code" name="promocode" required />
-        </div>
-        <div className="promo-container">
-          <input type="text" placeholder = "Expiration Date" name="expiredate" required />
-        </div>
-        <div className="promoButton-container">
-          <input type="submit"/>
-        </div>
-      </form>
           </div>
         );
       };
