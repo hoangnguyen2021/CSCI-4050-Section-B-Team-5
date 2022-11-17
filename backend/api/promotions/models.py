04c4e9644cca035bccf78d1838a8f0fb6f4a7e0a
kaczynski-tech-tips <nameissubjecttochange@gmail.com>
2022-11-17 12:49:38 -0500

    fixed promotion models

--- backend/api/promotions/models.py
+++ backend/api/promotions/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Promotions(models.Model):
     promotion_info = models.CharField(max_length = 300)
-    promo_code = models.CharField(max_length = 300)
-    is_active = models.BooleanField()
+    promo_code = models.CharField(max_length = 8)
+    is_active = models.BooleanField(default="false")
     percentage = models.FloatField(max = 100.00)
