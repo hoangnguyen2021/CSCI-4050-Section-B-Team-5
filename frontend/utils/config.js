b84b1cc0fefb2acd8f2dda42a4b7e59f76ebcfee
Hoang Vu Nguyen <hoangvu.nguyen@washpost.com>
2022-11-17 14:32:01 -0500

    Fix add promotion ui

---
+++ frontend/utils/config.js
@@ -0,0 +1,15 @@
+export const navs = [
+    { id: 0, text: "Home", href: "/" },
+    { id: 1, text: "Manage Users", href: "/manageUsers" },
+    { id: 2, text: "Manage Movies", href: "/manage-movies" },
+    { id: 3, text: "Add Movie", href: "/movie-form" },
+    { id: 4, text: "Promotions", href: "/promotions" },
+];
+
+export const ratings = [
+    { id: 0, name: "G" },
+    { id: 1, name: "PG" },
+    { id: 2, name: "PG-13" },
+    { id: 3, name: "R" },
+    { id: 4, name: "NC-17" },
+];
