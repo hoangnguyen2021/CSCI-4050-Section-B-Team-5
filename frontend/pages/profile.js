e8b267f8b08bdf68a4f358ba7dee031f3da67ca0
Hoang Vu Nguyen <hoangvu.nguyen@washpost.com>
2022-11-21 17:36:51 -0500

    Fix build error

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 export default function Example() {
   return (
     <div className="overflow-hidden bg-black shadow">
-        <div className="text-center">
+      <div className="text-center">
         <Link href="/">
-    <button className="home"> Back to Home</button>
-    </Link>
-    </div>
+          <button className="home"> Back to Home</button>
+        </Link>
+      </div>
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg font-medium leading-6 text-white-900">Applicant Information</h3>
         <p className="mt-1 max-w-2xl text-sm text-white-500">Personal details and application.</p>
@@ -40,11 +40,11 @@ export default function Example() {
           </div>
           <div className="bg-black px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dd className="mt-1 text-sm text-white-900 sm:col-span-2 sm:mt-0">
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="/editProfile" className="font-medium text-red-600 hover:text-red-500">
-                      Edit Profile
-                    </a>
-                  </div>
+              <div className="ml-4 flex-shrink-0">
+                <Link href="/editProfile" >
+                  <div className="font-medium text-red-600 hover:text-red-500">Edit Profile</div>
+                </Link>
+              </div>
             </dd>
           </div>
         </dl>
