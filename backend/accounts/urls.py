82494d0d4c56b610ff7a182e99b7d6a7f48dff5b
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-11-22 05:54:53 -0500

    actualty displays the user's name

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
   path('' , UserViewSet.as_view({'get' : 'get_user_list'}) ),
   path('deactivate' , UserViewSet.as_view({'get' : 'deactivate_user'})),
   path('updateuserinfo' , UserViewSet.as_view({'patch' : 'update_user'})),
-  path('isadmin' , GetAdmin.as_view({'get':'is_admin'}))
+  path('isadmin' , GetAdmin.as_view({'get':'is_admin'})),
+  path('username' , UserViewSet.as_view({'get':'return_user_name'})),
+
 ]
