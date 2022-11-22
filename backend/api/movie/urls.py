0ec26c7a7adbd8c635e427a42a7bfb4f154e16f4
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-11-22 04:38:54 -0500

    filter fixes

--- backend/api/movie/urls.py
+++ backend/api/movie/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list' , views.MovieReadSet.as_view({"get":"list"})),
     path('update' , views.MovieViewSet.as_view({"patch" : "update"}) ),
     path('search_movie_title_alt', views.MovieSearchSet.as_view({"get":"search_movie_title_alt"})),
-    path('search_movie_category_al', views.MovieSearchSet.as_view({"get":"search_movie_category_al"})),
+    path('search_movie_category_alt', views.MovieSearchSet.as_view({"get":"search_movie_category_alt"})),
     path('filter_director', views.MovieSearchSet.as_view({"get":"director"})),
     path('get_movie_by_id' , views.MovieSearchSet.as_view({"get":"get_movie_by_id"})),
 
