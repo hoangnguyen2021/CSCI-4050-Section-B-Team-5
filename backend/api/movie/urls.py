8843d0f14156833ad4cfd30c55b4f4542f91553b
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-11-22 04:35:15 -0500

    filter category fix(?) and search title fix

--- backend/api/movie/urls.py
+++ backend/api/movie/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('list' , views.MovieReadSet.as_view({"get":"list"})),
     path('update' , views.MovieViewSet.as_view({"patch" : "update"}) ),
     path('search_movie_title_alt', views.MovieSearchSet.as_view({"get":"search_movie_title_alt"})),
-    path('filter_category', views.MovieSearchSet.as_view({"get":"category"})),
+    path('search_movie_category_al', views.MovieSearchSet.as_view({"get":"search_movie_category_al"})),
     path('filter_director', views.MovieSearchSet.as_view({"get":"director"})),
     path('get_movie_by_id' , views.MovieSearchSet.as_view({"get":"get_movie_by_id"})),
 
