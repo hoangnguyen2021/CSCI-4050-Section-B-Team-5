dc60dbed1d7ecf4dd9a80b3b6080ea5ff1815915
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-11-22 04:47:20 -0500

    changed category filter to list

--- frontend/pages/search-by-category/index.js
+++ frontend/pages/search-by-category/index.js
@@ -20,7 +20,7 @@ const SearchByCategory = () => {
 
     const getMovies = async () => {
         try {
-            const response = await get("api/movie/search_movie_category_alt");
+            const response = await get("api/movie/list");
             const responseData = response.data;
             if (responseData) {
                 setMovies(responseData.map(movie => {
