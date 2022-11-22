d56273b52b46059d20b867aaedd70871bc574e61
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2022-11-22 04:47:20 -0500

    changed category filter to list

--- backend/api/movie/urls.py
+++ backend/api/movie/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('create' , views.MovieViewSet.as_view({"post":"create"})),
     path('list' , views.MovieReadSet.as_view({"get":"list"})),
     path('update' , views.MovieViewSet.as_view({"patch" : "update"}) ),
-    path('search_movie_title_alt', views.MovieSearchSet.as_view({"get":"search_movie_title_alt"})),
     path('search_movie_category_alt', views.MovieSearchSet.as_view({"get":"search_movie_category_alt"})),
     path('filter_director', views.MovieSearchSet.as_view({"get":"director"})),
     path('get_movie_by_id' , views.MovieSearchSet.as_view({"get":"get_movie_by_id"})),
