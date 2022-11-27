5eb4011a961d0ac13de61af089a157f07ec52362
Guvvala-Prasanth-Reddy <qtpython12@gmail.com>
2022-11-27 03:27:57 -0500

    removed extra info from showroom

--- backend/api/ticket/models.py
+++ backend/api/ticket/models.py
@@ -5,9 +5,7 @@ from booking.models import Booking
 # Create your models here.
 
 class Ticket(models.Model):
-    UserAccount = models.ForeignKey(UserAccount , on_delete=models.CASCADE)
     AgeGroup = models.CharField(max_length=30)
-    Show = models.ForeignKey(Show , on_delete=models.CASCADE)
     Seat_no = models.IntegerField()
     Ticket_holder_name = models.CharField(max_length=100)
     Booking_Id = models.ForeignKey(Booking , on_delete=models.CASCADE)
