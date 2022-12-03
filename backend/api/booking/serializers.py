ebf12cebae409eeb77044e2bec89b45967d150fd
Guvvala-Prasanth-Reddy <qtpython12@gmail.com>
2022-12-03 17:41:00 -0500

    removed booking and tickets

--- backend/api/booking/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Booking
-
-class BookingSerializer(serializers.ModelSerializer):
-    class Meta:
-            model = Booking
-            fields= '__all__'
