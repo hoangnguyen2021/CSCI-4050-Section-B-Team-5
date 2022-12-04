c9b0ca5de7cd13297dd9223a747886dbf5038dd5
Guvvala-Prasanth-Reddy <qtpython12@gmail.com>
2022-12-04 15:38:15 -0500

    made a change

--- backend/api/booked_seats_tracker/utils.py
+++ backend/api/booked_seats_tracker/utils.py
@@ -9,7 +9,7 @@ def CreateBookedSeatsInstance(start_date , end_date , show_id):
     print(start_date , end_date)
     delta_date_object = end_date-start_date
     delta = max( delta_date_object.days , 0)
-    for i in range(1, delta+1):
+    for i in range(0, delta+1):
         booked_seats  = BookedSeats(show_date = start_date + datetime.timedelta(days = i) , show_id = Show.objects.get(id = show_id) )
         booked_seats.save()
 def block_seats_and_return_price(id ,  tickets ):
