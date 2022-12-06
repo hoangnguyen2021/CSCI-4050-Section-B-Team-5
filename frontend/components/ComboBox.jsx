76baaf93ac4ab82ac13b35dd538dfdb33fd8945d
Hoang Vu Nguyen <hoangvu.nguyen@washpost.com>
2022-12-05 23:19:22 -0500

    Finish booking

--- frontend/components/ComboBox.jsx
+++ frontend/components/ComboBox.jsx
@@ -5,104 +5,96 @@ import { classNames } from "../utils/utils";
 import Link from "next/link";
 
 const ComboBox = ({ label, options, selected, setSelected }) => {
-    const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
 
-    const filteredOptions =
-        query === ""
-            ? options
-            : options.filter((option) => {
-                  return option.name
-                      .toLowerCase()
-                      .includes(query.toLowerCase());
-              });
+  const filteredOptions =
+    query === ""
+      ? options
+      : options.filter((option) => {
+          return option.name.toLowerCase().includes(query.toLowerCase());
+        });
 
-    return (
-        <Combobox as="div" value={selected} onChange={setSelected}>
-            {({ open }) => (
-                <div>
-                    <Combobox.Label className="text-on-primary text-base font-medium">
-                        {label}
-                    </Combobox.Label>
-                    <div className="relative mt-1">
-                        <Combobox.Input
-                            className="relative bg-transparent w-full cursor-default border-0 border-b border-on-primary py-2 pl-3 pr-10 text-base focus:border-0 focus:border-b-2 focus:border-on-primary focus:ring-0"
-                            onChange={(e) => setQuery(e.target.value)}
-                            displayValue={(option) => option.name}
-                        />
-                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center px-2 focus:outline-none">
-                            <ChevronUpDownIcon
-                                className="h-6 w-6 text-on-primary"
-                                aria-hidden="true"
-                            />
-                        </Combobox.Button>
+  return (
+    <Combobox as="div" value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <div>
+          <Combobox.Label className="text-on-primary text-base font-medium">
+            {label}
+          </Combobox.Label>
+          <div className="relative mt-1">
+            <Combobox.Input
+              className="relative bg-transparent w-full cursor-default border-0 border-b border-on-primary py-2 pl-3 pr-10 text-base focus:border-0 focus:border-b-2 focus:border-on-primary focus:ring-0"
+              onChange={(e) => setQuery(e.target.value)}
+              displayValue={(option) => option.name}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center px-2 focus:outline-none">
+              <ChevronUpDownIcon
+                className="h-6 w-6 text-on-primary"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
 
-                        <Transition
-                            show={open}
-                            as={Fragment}
-                            enter="transition duration-100 ease-out"
-                            enterFrom="transform scale-95 opacity-0"
-                            enterTo="transform scale-100 opacity-100"
-                            leave="transition duration-75 ease-out"
-                            leaveFrom="transform scale-100 opacity-100"
-                            leaveTo="transform scale-95 opacity-0"
-                        >
-                            <Combobox.Options className="absolute z-10 mt-1 max-h-36 w-full overflow-auto bg-background border py-1 focus:outline-none sm:text-sm">
-                                {filteredOptions.map((option) => (
-                                    <Combobox.Option
-                                        key={option.id}
-                                        value={option}
-                                        className={({ active }) =>
-                                            classNames(
-                                                active
-                                                    ? "bg-primary text-on-primary"
-                                                    : "text-on-primary",
-                                                "relative cursor-default select-none text-base py-2 pl-3 pr-9"
-                                            )
-                                        }
-                                    >
-                                        {({ active, selected }) => (
-                                            <>
-                                                <div className="flex items-center">
-                                                    <img
-                                                        src={option.imageUrl}
-                                                        alt=""
-                                                        className="h-6 w-6 flex-shrink-0 rounded-full"
-                                                    />
-                                                    <span
-                                                        className={classNames(
-                                                            "ml-3 truncate",
-                                                            selected &&
-                                                                "font-semibold"
-                                                        )}
-                                                    >
-                                                        {option.name}
-                                                    </span>
-                                                </div>
+            <Transition
+              show={open}
+              as={Fragment}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Combobox.Options className="absolute z-10 mt-1 max-h-36 w-full overflow-auto bg-background border py-1 focus:outline-none sm:text-sm">
+                {filteredOptions.map((option) => (
+                  <Combobox.Option
+                    key={option.id}
+                    value={option}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "bg-primary" : "",
+                        "relative cursor-default select-none text-on-primary text-base py-2 pl-3 pr-9"
+                      )
+                    }
+                  >
+                    {({ active, selected }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img
+                            src={option.imageUrl}
+                            alt=""
+                            className="h-6 w-6 flex-shrink-0 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              "ml-3 truncate",
+                              selected && "font-semibold"
+                            )}
+                          >
+                            {option.name}
+                          </span>
+                        </div>
 
-                                                {selected && (
-                                                    <span
-                                                        className={classNames(
-                                                            "absolute inset-y-0 right-0 flex items-center text-on-primary pr-4",
-                                                            active ? "" : ""
-                                                        )}
-                                                    >
-                                                        <CheckIcon
-                                                            className="h-5 w-5"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </span>
-                                                )}
-                                            </>
-                                        )}
-                                    </Combobox.Option>
-                                ))}
-                            </Combobox.Options>
-                        </Transition>
-                    </div>
-                </div>
-            )}
-        </Combobox>
-    );
+                        {selected && (
+                          <span
+                            className={classNames(
+                              "absolute inset-y-0 right-0 flex items-center text-on-primary pr-4",
+                              active ? "" : ""
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        )}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        </div>
+      )}
+    </Combobox>
+  );
 };
 
 export default ComboBox;
